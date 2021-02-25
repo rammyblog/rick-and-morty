cbb9647afb3f0f8aebf3fa627532ce54f6900b42
Onasanya Babatunde <Onasanyababatunde67@gmail.com>
2021-02-25 19:55:04 +0100

    Setup useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Store } from './Store';
+function App(): JSX.Element {
+    const store = React.useContext(Store);
+    return (
+        <>
+            <h1>Rick and Morty</h1>
+            <p>Pick your Favorite episode!!</p>
+        </>
+    );
 }
 
 export default App;
