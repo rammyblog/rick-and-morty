62c28f8f5835300f100e6847b1eea44d7cec3620
Onasanya Babatunde <Onasanyababatunde67@gmail.com>
2021-02-26 00:41:50 +0100

    Setup store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Store } from './Store';
 function App(): JSX.Element {
-    const store = React.useContext(Store);
+    const { state, dispatch } = React.useContext(Store);
     return (
         <>
             <h1>Rick and Morty</h1>
--- src/Store.tsx
+++ src/Store.tsx
@@ -5,19 +5,30 @@ interface IState {
     favorites: [];
 }
 
+interface IEpisode {
+    type: string;
+    payload: any;
+}
+
 const initialState: IState = {
     episodes: [],
     favorites: [],
 };
 
-export const Store = React.createContext<IState>(initialState);
+export const Store = React.createContext<IState | any>(initialState);
 
-function reducer() {
-    // pass
+function reducer(state: IState, action: IEpisode): IState {
+    switch (action.type) {
+        case 'FETCH_DATA':
+            return { ...state, episodes: action.payload };
+        default:
+            return state;
+    }
 }
 
 export function StoreProvider(props: any): JSX.Element {
+    const [state, dispatch] = React.useReducer(reducer, initialState)
     return (
-        <Store.Provider value={initialState}>{props.children}</Store.Provider>
+        <Store.Provider value={{state, dispatch}}>{props.children}</Store.Provider>
     );
 }
